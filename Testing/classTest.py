f77b390a6ca7dd9cff30ef6ad45956a3736bb28b
BroboticsInc31 <broboticsinc31@gmail.com>
2019-03-15 22:02:16 -0500

    Updated test

--- Testing/classTest.py
+++ Testing/classTest.py
@@ -24,17 +24,32 @@ print("Finding an ODrive...")
 #my_drive2 = odrive.find_any("usb","388937803437")
 lilbro.findDrivers(206237793548,388937803437)
 
-lilbro.setState(1)
+print("Initial gain is ", lilbro.driver1.axis0.controller.config.pos_gain)
+lilbro.setVelLim(60000)
+lilbro.setState(8)
+
+print("Initial velocity limit is ",lilbro.driver1.axis0.controller.config.vel_limit)
 
 while True:
     try:
-        print("D1: ",lilbro.getDriver1())
-        time.sleep(1)
+      #  pos1 = lilbro.getCounts()
+      #  print("D1: ",lilbro.getDriver1())
+      #  print("Positions: ",lilbro.getCounts())
+      #  print("Motor Pos1 = ", pos1[1])
+      #  print("Motor currents: ",lilbro.getCurrents())
+        
+#        lilbro.setGain(50)
+#        print("Gain is ", lilbro.driver1.axis0.controller.config.pos_gain)
+
+        print("Error: ",lilbro.isError())
+
+        time.sleep(1.5)
 
     except (KeyboardInterrupt):
         # Turn off the motors
         # Close the data file
-        testdata.close()
+
+        lilbro.setState(1)
         sys.exit()
 
 
