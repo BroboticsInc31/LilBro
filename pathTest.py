7bc255f2f08124416ca4b416bf90ffe676112b07
BroboticsInc31 <broboticsinc31@gmail.com>
2019-03-29 00:42:15 -0500

    Updated pathTest

--- pathTest.py
+++ pathTest.py
@@ -5,14 +5,19 @@ import time
 
 lilbro = robot.robot()
 alpha2 = []
-l = np.linspace(0.14,0.22)
+legParms = []
+l = np.linspace(0.14,0.22,50)
 alpha = 0
 l1 = 0.1
 l2 = 0.22
+i = 0
+
+print(len(l))
 
 for i in range(len(l)):
-    alpha2[i] = math.acos(((l1**2+l[i]**2-l2**2)/(2*l1*l[i])))
+    print(l[i])
+    alpha2.append(math.acos((((l1**2)+((l[i])**2)-(l2**2))/(2*l1*l[i]))))
     alpha1 = alpha - math.pi/2
 
-    legParms = lilbro.symmetric(alpha1,alpha2,l1,l2)
-    print("Foot positions ",legParms[4][1]," and ",legParms[4][2])
+    legParms.append(lilbro.symmetric(alpha1,alpha2[i],l1,l2))
+    print("Foot positions ",legParms[i])
