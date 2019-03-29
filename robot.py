c0a0830e52b0fe18c26e8245dc3f4d1517842458
zuhairfarra <45976247+zuhairfarra@users.noreply.github.com>
2019-03-28 22:33:03 -0500

    Updated robot.py with symmetric

--- robot.py
+++ robot.py
@@ -38,8 +38,11 @@ class robot():
         self.pGain = 20
         self.driverCurLim = 50
         self.driverVelLim = 60000
+
         self.driver1 = 0
         self.driver2 = 0
+        self.driver3 = 0
+        self.driver4 = 0
 
         global timeArray
         timeArray = []
@@ -69,19 +72,21 @@ class robot():
     def getDriver2(self):
         return self.driver2.serial_number
 
-    def setState(self,state):
-        self.driver1.axis0.requested_state = state
-        self.driver1.axis1.requested_state = state
+    def setState(self,xdriver,xstate):
+        xdriver.axis0.requested_state = xstate
+        xdriver.axis1.requested_state = xstate
+    
+    def setStates(self,state):
+        self.setState(self.driver1,state)
+        self.setState(self.driver2,state)
 
-        self.driver2.axis0.requested_state = state
-        self.driver2.axis1.requested_state = state
+    def setGain(self,xdriver,xgain):
+        xdriver.axis0.controller.config.pos_gain = xgain
+        xdriver.axis1.controller.config.pos_gain = xgain
 
-    def setGain(self,newGain):
-        self.driver1.axis0.controller.config.pos_gain = newGain
-        self.driver1.axis1.controller.config.pos_gain = newGain
-
-        self.driver2.axis0.controller.config.pos_gain = newGain
-        self.driver2.axis1.controller.config.pos_gain = newGain
+    def setGains(self,newGain):
+        self.setGain(self.driver1,newGain)
+        self.setGain(self.driver2,newGain)
 
     def addGain(self,incGain):
         self.driver1.axis0.controller.config.pos_gain += incGain
@@ -96,31 +101,45 @@ class robot():
         else:
             return 0
 
-    def setVelLim(self,velLim):
-        self.driver1.axis0.controller.config.vel_limit = velLim
-        self.driver1.axis1.controller.config.vel_limit = velLim
 
-        self.driver2.axis0.controller.config.vel_limit = velLim
-        self.driver2.axis1.controller.config.vel_limit = velLim
+    def setVelLim(self,xdriver,xlim):
+        xdriver.axis0.controller.config.vel_limit = xlim
+        xdriver.axis1.controller.config.vel_limit = xlim
 
-    def setCurLim(self,curLim):
-        self.driver1.axis0.motor.config.current_lim = curLim
-        self.driver1.axis1.motor.config.current_lim = curLim
+    def setVelLims(self,velLim):
+        self.setVelLim(self.driver1,velLim)
+        self.setVelLim(self.driver2,velLim)
 
-        self.driver2.axis0.motor.config.current_lim = curLim
-        self.driver2.axis1.motor.config.current_lim = curLim
+
+    def setCurLim(self,xdriver,xlim):
+        xdriver.axis0.motor.config.current_lim = xlim
+        xdriver.axis1.motor.config.current_lim = xlim
+
+    def setCurLims(self,curLim):
+        self.setCurLim(self.driver1,curLim)
+        self.setCurLim(self.driver2,curLim)
+
+
+    def getCount(self,driver):
+        return [self.driver.axis0.encoder.pos_estimate,self.driver.axis1.encoder.pos_estimate];
 
     def getCounts(self):
         return [self.driver1.axis0.encoder.pos_estimate,self.driver1.axis1.encoder.pos_estimate,self.driver2.axis0.encoder.pos_estimate,self.driver2.axis1.encoder.pos_estimate];
-        
+
+
+    def getCurrent(self,driver):
+        return [self.driver.axis0.motor.current_control.Iq_measured,self.driver.axis1.motor.current_control.Iq_measured]
+
     def getCurrents(self):
         return [self.driver1.axis0.motor.current_control.Iq_measured,self.driver1.axis1.motor.current_control.Iq_measured,self.driver2.axis0.motor.current_control.Iq_measured,self.driver2.axis1.motor.current_control.Iq_measured];
+
 
     def getBusVoltage(self):
         return [self.driver1.vbus_voltage,self.driver2.vbus_voltage];
 
     def getTemp(self):
         return [self.driver1.axis0.get_temp(),self.driver1.axis1.get_temp(),self.driver2.axis0.get_temp()],self.driver2.axis1.get_temp()];
+
 
     def writeToFile(self,saved):
         if(globals.dataOn == 1):
@@ -130,6 +149,7 @@ class robot():
         elif(globals.dataOn == 0 and saved == 0):
             np.savetxt('ClassPositions.txt',np.c_[timeArray,motor1],fmt="%.3f %.3f")
             saved = 1
+
 
     def toDeg(self,counts):
         return 360*(counts)/8192
@@ -143,138 +163,27 @@ class robot():
     def toMotor(self,counts):
         return counts*5
 
-    #    def writeToFile(self)
-  #        startTime = time.time()
-  #        motorPos = self.getCounts()
 
-  #      np.savetxt('startSequence.txt',np.c_[array2,array3],fmt="%.3f %.3f")
+    def takeStep(self,driver,pos):
+        driver.axis0.controller.pos_setpoint = pos
+        driver.axis1.controller.pos_setpoint = pos
+
+    def checkStep(self,driver,deg):
+        if()
   
-    # * SetTunings(...)*************************************************************
-    # * This function allows the controller's dynamic performance to be adjusted.
-    # * it's called automatically from the constructor, but tunings can also
-    # * be adjusted on the fly during normal operation
-    # ******************************************************************************/
+  
+    def rotation(self,theta):
+        return R = np.array([[math.cos(math.radians(theta)),-math.sin(math.radians(theta))],[math.sin(math.radians(theta)),math.cos(math.radians(theta))]])
 
-    #def SetTunings(self, Kp, Ki, Kd, POn):
-    #    if (Kp < 0) or (Ki < 0) or (Kd < 0):
-    #        return
+    def symmetric(alpha1,alpha2,l1,l2):
+        base = [0,0]
+        leftshoulder = np.array([l1*math.cos(math.radians(alpha2)),l1*math.sin(math.radians(alpha2))])
+        rightshoulder = np.array([l1*math.cos(math.radians(alpha2)),-l1*math.sin(math.radians(alpha2))])
+        foot = np.array([l1*math.cos(math.radians(alpha2))+math.sqrt((l2**2)-(l1**2)*(math.sin(math.radians(alpha2)))**2),0])
 
-    #    self.pOn = POn
-    #    self.pOnE = (POn == P_ON_E)
+        R = rotation(alpha1)
+        leftshoulder = R.dot(leftshoulder)
+        rightshoulder = R.dot(rightshoulder)
+        foot = R.dot(foot)
 
-    #    self.dispKp = Kp
-    #    self.dispKi = Ki
-    #    self.dispKd = Kd
-
-    #    SampleTimeInSec = self.SampleTime
-
-    #    self.kp = Kp
-    #    self.ki = Ki * SampleTimeInSec
-    #    self.kd = Kd / SampleTimeInSec
-
-    #    if (self.controllerDirection == REVERSE):
-    #        self.kp = -self.kp
-    #        self.ki = -self.ki
-    #        self.kd = -self.kd
-
-    ## * SetTunings(...)*************************************************************
-    # * Set Tunings using the last-rembered POn setting
-    # ******************************************************************************
-    # def SetTunings(self, Kp, Ki, Kd):
-    #     SetTunings(Kp, Ki, Kd, self.pOn)
-
-    # * SetSampleTime(...) *********************************************************
-    # * sets the period, in Milliseconds, at which the calculation is performed
-    # ******************************************************************************
-    #def SetSampleTime(self, NewSampleTime):
-    #    NewSampleTime = float(NewSampleTime)
-
-    #    if NewSampleTime > 0.0:
-    #        ratio = NewSampleTime / self.SampleTime
-
-    #        self.ki *= ratio
-    #        self.kd /= ratio
-    #        self.SampleTime = NewSampleTime
-
-    ## * SetOutputLimits(...)****************************************************
-    # *     This function will be used far more often than SetInputLimits.  while
-    # *  the input to the controller will generally be in the 0-1023 range (which is
-    # *  the default already,)  the output will be a little different.  maybe they'll
-    # *  be doing a time window and will need 0-8000 or something.  or maybe they'll
-    # *  want to clamp it from 0-125.  who knows.  at any rate, that can all be done
-    # *  here.
-    # **************************************************************************
-    #def SetOutputLimits(self, Min,Max):
-    #    if Min > Max:
-    #        return
-
-    #    self.outMin = Min
-    #    self.outMax = Max
-
-    #    if self.inAuto:
-    #        if self.output > self.outMax:
-    #            self.output = self.outMax
-    #        elif self.output < self.outMin:
-    #            self.output = self.outMin
-
-    #        if self.outputSum > self.outMax:
-    #            self.outputSum = self.outMax
-    #        elif self.outputSum < self.outMin:
-    #            self.outputSum = self.outMin
-
-    ## * SetMode(...)****************************************************************
-    # * Allows the controller Mode to be set to manual (0) or Automatic (non-zero)
-    # * when the transition from manual to auto occurs, the controller is
-    # * automatically initialized
-    # ******************************************************************************
-    #def SetMode(self, Mode):
-    #    newAuto = (Mode == AUTOMATIC)
-
-    #    if newAuto and (self.inAuto == False):
-    #        self.Initialize()
-
-    #    self.inAuto = newAuto
-
-    # * Initialize()****************************************************************
-    # *  does all the things that need to happen to ensure a bumpless transfer
-    # *  from manual to automatic mode.
-    # ******************************************************************************
-    #def Initialize(self):
-    #    self.outputSum = self.output
-
-    #    if self.outputSum > self.outMax:
-    #        self.outputSum = self.outMax
-    #    elif self.outputSum < self.outMin:
-    #        self.outputSum = self.outMin
-
-    ## * SetControllerDirection(...)*************************************************
-    # * The PID will either be connected to a DIRECT acting process (+Output leads
-    # * to +Input) or a REVERSE acting process(+Output leads to -Input.)  we need to
-    # * know which one, because otherwise we may increase the output when we should
-    # * be decreasing.  This is called from the constructor.
-    # ******************************************************************************
-    #def SetControllerDirection(self, Direction):
-    #    if self.inAuto and (Direction != controllerDirection):
-    #        self.kp = -self.kp
-    #        self.ki = -self.ki
-    #        self.kd = -self.kd
-    #    self.controllerDirection = Direction
-
-    #def SetSetpoint(self, newSetpoint):
-    #    self.setpoint = newSetpoint
-
-    ## * Status Funcions*************************************************************
-    # * Just because you set the Kp=-1 doesn't mean it actually happened.  these
-    # * functions query the internal state of the PID.  they're here for display
-    # * purposes.  this are the functions the PID Front-end uses for example
-    # ******************************************************************************
-    #def GetKp(self):
-    #    return self.dispKp
-    #def GetKi(self):
-    #    return self.dispKi
-    #def GetKd(self):
-    #    return self.dispKd
-    #def GetMode():
-    #    return (MANUAL, AUTOMATIC)[self.inAuto == True]
-    #def GetDirection():
-    #    return self.ControllerDirection
+        return [base,leftshoulder,rightshoulder,foot]; 
